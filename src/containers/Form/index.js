7ef7ef0ee7be830f46c0ed0de4e9ea065ebf482a
Imane Baqua <imane.baqua@hotmail.fr>
2024-04-21 18:21:54 +0200

    correction erreur console pour vérifier si last est bien undefined

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,7 +4,10 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+const mockContactApi = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 500);
+  }); // reduction du setTimeout car sinon le test passe en failed car le test attend max 1s
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
@@ -14,7 +17,7 @@ const Form = ({ onSuccess, onError }) => {
       setSending(true);
       // We try to call mockContactApi
       try {
-         await mockContactApi(); // Attendez la résolution de la promesse
+        await mockContactApi(); // Attendez la résolution de la promesse
         setSending(false);
         // Appeler onSuccess ici après le succès de l'envoi
         onSuccess(); // Appel de la fonction onSuccess
@@ -58,11 +61,11 @@ const Form = ({ onSuccess, onError }) => {
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-}
+};
 
 export default Form;
